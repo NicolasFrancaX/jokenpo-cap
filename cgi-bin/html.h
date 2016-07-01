3dad137ac1888c56eb99071a3b3465b24fc3c533
Nicolas França <nicolasfranca9@gmail.com>
2016-07-01 14:45:15 -0300

    Modularizando código - HTML em arquivo próprio pra isso.

---
+++ cgi-bin/html.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void condicaoHTML(int matriz[3][3], int player, int cpu) {
+    switch(matriz[player][cpu]) {
+        case 0:
+            printf("<h1>Você perdeu!</h1>");
+            break;
+        case 1:
+            printf("<h1>Você empatou!</h1>");
+            break;
+        case 2:
+            printf("<h1>Você ganhou!</h1>");
+            break;
+    }
+}
+
+void HTML(char strings[3][10], int matriz[3][3], int player, int cpu) {
+    printf("%s%c%c\n", "Content-Type:text/html;charset=UTF-8", 13, 10);
+    printf("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
+    printf("<html>");
+    printf("<head>");
+    printf("<meta charset=\"utf-8\" />");
+    printf("<title>Jo-ken-pô!</title>");
+    printf("<link href=\"../style.css\" rel=\"stylesheet\" type=\"text/css\" />");
+    printf("<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>");
+    printf("<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>");
+    printf("</head>");
+    printf("<body>");
+
+    condicaoHTML(matriz, player, cpu);
+
+    printf("<div id=\"escolha\">");
+    printf("<p> Você:       &nbsp; &nbsp; &nbsp;       PC:</p>");
+    printf("</div>");
+    printf("<div id=\"imagens\">");
+    printf("<img src=\"../images/%s_jogador.png\">", strings[player]);
+    printf("<img src=\"../images/%s_cpu.png\">", strings[cpu]);
+    printf("</div>");
+    printf("</div>");
+    printf("</body>");
+    printf("</html>");
+}
